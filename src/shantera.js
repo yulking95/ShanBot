88a1814e362b1c198d599d3c8894ea1c1e670179
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-06-25 15:18:16 -0500

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -2,43 +2,12 @@ const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
 
 
-- todo bien
-- buenos dias
-- bot gay
-- gracias
-- hola
-- fua
-- corte
-- gaspi buenos dias 
-- gaspi me saludas
-- gaspi y las minitas
-- gaspi todo bien
-- me quiero suicidar
-- gaspi ya no aguanto
-- contate algo bot
-- sexo
-- momento epico
-- el bot del orto no funciona
-- epicardo
-- insta de la minita
-- una mierda de bot
-- ultimo momento
-- nefasto
-- paraguayo
-- bot de mierda
-- venezolano
-- gaspi corte
-- ya me voy a dormir
-- calefon
-- apurate bot
-- un chino
-- no funciona
-- boliviano
+- En creación...
 
 
-_Ojito escribe tal y como esta en el mensaje_
+_sapo_
 
-by shanduy`
+by ⚡HH-BOT⚡`
 
 }
 
