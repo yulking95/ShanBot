512ab3f00feadc04c8ea90b373be9e1e09a00f92
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-06-27 21:00:54 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -3,15 +3,17 @@ const desmenu = (prefix, pushname) => {
 *Comandos De Descargas ✅*
  
 - ${prefix}play
-  Coloca el nombre de la cancion. O el link del video YT
+  Coloca el nombre de la cancion. 
+  O el link del video YT
 - ${prefix}ytmp3
+
   Coloca el link del video YT
 - ${prefix}ytmp4
   Coloca el link del video YT
     
 _*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_
      
-by HHBOT`
+`
 
 }
 
