1864b22c7720b715cd5629f8d1ab4f156e05e463
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-06-27 21:01:25 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -11,9 +11,9 @@ const desmenu = (prefix, pushname) => {
 - ${prefix}ytmp4
   Coloca el link del video YT
     
-_*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_
+_*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_`
      
-`
+
 
 }
 
