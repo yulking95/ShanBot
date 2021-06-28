d9fc6ec2d19c1457eec4c57f1889c6507c1d3267
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-06-27 21:27:05 -0500

    Update otak.js

--- src/otak.js
+++ src/otak.js
@@ -2,11 +2,11 @@ const otak = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
 
 
-- gime
-- sexo
-- 
-- onichan
-- 
+ gime
+ sexo
+ Pasen porno
+ onichan
+ 
 
 
 _Ojo escribe tal como esta_
